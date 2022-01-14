7718df3fc099fe5483c5b924c71a4e6e7d710c8f
Petia Kortcheva <peetiak@gmail.com>
2022-01-14 13:14:33 -0800

    Add text to README.md file

---
+++ README.md
@@ -0,0 +1,31 @@
+# _{Welcome to the World of Programming}_
+
+#### By _**{Petia Kortcheva}**_
+
+#### _{A simple website for learnig about 3 Program Languages}_
+
+## Technologies Used
+
+* _HTML_
+* _JavaScript_
+* _CSS_
+* _jQuery_
+
+## Description
+
+_{This application allows users to find basic information about 3 Program Languages, C#, JavaScript and Python and allows them to take a short Survey at the end which pairs them with the right Program Language for them.}_
+
+## Setup/Installation Requirements
+
+* _Copy and paste this URL on your computer_
+
+## Known Bugs
+
+* _Any known issues_
+* _should go here_
+
+## License
+
+_{MIT}_
+
+Copyright (c)  2022 Petia Kortcheva
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
   $("form#program-language-suggester").submit(function(event) {
-    const loveVideoGames = parseInt($("input#YesorNo").val());
-    const loveToDesign = parseInt($("input#YesorNo").val());
-    const loveCoffee = $("select#YesorNo").val();
-    const loveTea= parseInt($("input#YesorNo").val());
-    const loveDogs = $("select#YesorNo").val();
+    const loveVideoGames = confirm ("input#YesorNo").val();
+    const loveToDesign = confirm ("input#YesorNo").val();
+    const loveCoffee = confirm ("select#YesorNo").val();
+    const loveTea= confirm ("input#YesorNo").val();
+    const loveDogs = confirm ("select#YesorNo").val();
     $("#C#").hide();
     $("#JavaScript").hide();
     $("#Python").hide();
     console.log(age);
     console.log(gender);
 
-    if (gender === 'female' && age > 21 && age <= 30) {
-      $("#BradPitt").show();
-     } else if (gender === 'female' && age > 31) {
-       $("#TomHanks").show();
+    if (loveVideoGames === 'Yes' && loveDogs === Yes && loveCoffee === Yes) {
+      $("C#").show();
+     } else if (loveToDesign === 'Yes' && LoveTea === 'Yes') {
+       $("#JavaScript").show();
      } else {
-       $("#SandraBullock").show();
+       $("#Python").show();
      }
      event.preventDefault();
   });
